2a32fa6e89cb72f20408ba36cd2f3b361d1ce614
THong <tianhongyap7185@gmail.com>
2021-06-30 11:20:05 +0800

    Initial commit

---
+++ app/src/main/java/com/mad/a21_linearlayout_using_the_layout_editor/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mad.a21_linearlayout_using_the_layout_editor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
